0f22f23665c3946ca4ebadb15c55f3f2c16d9e41
Mohammed Zaid <mohammedzee98@gmail.com>
2024-08-21 14:55:26 +0530

    Postgres DB connected

--- Postgres/src/utils.ts
+++ Postgres/src/utils.ts
@@ -1,5 +1,6 @@
 import { Client } from 'pg';
 
+// client allows us to connect to our Postgres instance for storing data.
 export async function getClient() {
     const client = new Client("postgres://wzsxsnxg:LHZ9Cv4QoZ1zctxapkOq2ch672-o9UQe@trumpet.db.elephantsql.com/wzsxsnxg");
     await client.connect();
