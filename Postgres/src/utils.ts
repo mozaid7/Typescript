5d3de8ac4f49b8c317b745472abe3c5ffa1c9cac
Mohammed Zaid <mohammedzee98@gmail.com>
2024-08-23 16:26:47 +0530

    bug

--- Postgres/src/utils.ts
+++ Postgres/src/utils.ts
@@ -2,7 +2,7 @@ import { Client } from 'pg';
 
 // client allows us to connect to our Postgres instance for storing data.
 export async function getClient() {
-    const client = new Client("postgres://wzsxsnxg:LHZ9Cv4QoZ1zctxapkOq2ch672-o9UQe@trumpet.db.elephantsql.com/wzsxsnxg");
+    const client = new Client("");
     await client.connect();
     return client;
 }
