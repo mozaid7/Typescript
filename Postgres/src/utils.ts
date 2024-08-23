dddc551c17709a354bca45dd10fe9a98d0eef102
Mohammed Zaid <mohammedzee98@gmail.com>
2024-08-23 16:18:13 +0530

    name changed

--- Postgres/src/utils.ts
+++ Postgres/src/utils.ts
@@ -1,11 +1,11 @@
 import { Client } from 'pg';
 import dotenv from 'dotenv';
 dotenv.config();
-const password = process.env.password;
+const key = process.env.password;
 
 // client allows us to connect to our Postgres instance for storing data.
 export async function getClient() {
-    const client = new Client(`postgres://avnadmin:${password}@pg-2cbdf470-mohammedzee98-2970.h.aivencloud.com:17452/todo?sslmode=require`);
+    const client = new Client(`postgres://avnadmin:${key}@pg-2cbdf470-mohammedzee98-2970.h.aivencloud.com:17452/todo?sslmode=require`);
     await client.connect();
     return client;
 }
