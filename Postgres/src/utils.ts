44e33f014aa573cf39ca83e5d53eba7b42396fa9
Mohammed Zaid <mohammedzee98@gmail.com>
2024-08-23 15:46:47 +0530

    database url connected

--- Postgres/src/utils.ts
+++ Postgres/src/utils.ts
@@ -2,7 +2,7 @@ import { Client } from 'pg';
 
 // client allows us to connect to our Postgres instance for storing data.
 export async function getClient() {
-    const client = new Client("postgres://wzsxsnxg:LHZ9Cv4QoZ1zctxapkOq2ch672-o9UQe@trumpet.db.elephantsql.com/wzsxsnxg");
+    const client = new Client("postgres://avnadmin:AVNS_Dd5xPtPAxXcMAt2hQwO@pg-2cbdf470-mohammedzee98-2970.h.aivencloud.com:17452/todo?sslmode=require");
     await client.connect();
     return client;
 }
