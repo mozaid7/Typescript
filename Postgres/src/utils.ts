a19adf90cc4d03bf32aeac47930af5fbd3f89d54
Mohammed Zaid <mohammedzee98@gmail.com>
2024-08-23 16:25:43 +0530

    bugs

--- Postgres/src/utils.ts
+++ Postgres/src/utils.ts
@@ -1,11 +1,8 @@
-import dotenv from 'dotenv';
 import { Client } from 'pg';
-dotenv.config();
-const key = process.env.password;
 
 // client allows us to connect to our Postgres instance for storing data.
 export async function getClient() {
-    const client = new Client(`postgres://avnadmin:${key}@pg-2cbdf470-mohammedzee98-2970.h.aivencloud.com:17452/todo?sslmode=require`);
+    const client = new Client("postgres://wzsxsnxg:LHZ9Cv4QoZ1zctxapkOq2ch672-o9UQe@trumpet.db.elephantsql.com/wzsxsnxg");
     await client.connect();
     return client;
 }
