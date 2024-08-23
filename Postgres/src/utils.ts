0bc9039a2ecad334e911ddc03a4db1c1c663d358
Mohammed Zaid <mohammedzee98@gmail.com>
2024-08-23 16:08:17 +0530

    env file added and dotenv added to hide the secrets

--- Postgres/src/utils.ts
+++ Postgres/src/utils.ts
@@ -1,8 +1,10 @@
 import { Client } from 'pg';
+require('dotenv').config();
+const password = process.env.password;
 
 // client allows us to connect to our Postgres instance for storing data.
 export async function getClient() {
-    const client = new Client("postgres://avnadmin:AVNS_Dd5xPtPAxXcMAt2hQwO@pg-2cbdf470-mohammedzee98-2970.h.aivencloud.com:17452/todo?sslmode=require");
+    const client = new Client(`postgres://avnadmin:${password}@pg-2cbdf470-mohammedzee98-2970.h.aivencloud.com:17452/todo?sslmode=require`);
     await client.connect();
     return client;
 }
