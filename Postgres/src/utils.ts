00ab1459e0d204276dc0401230e41955b0218ff1
Mohammed Zaid <mohammedzee98@gmail.com>
2024-08-23 21:22:20 +0530

    env variables exported to file

--- Postgres/src/utils.ts
+++ Postgres/src/utils.ts
@@ -1,8 +1,14 @@
+import dotenv from 'dotenv';
+dotenv.config();
 import { Client } from 'pg';
+
+const key = process.env.Password;
 
 // client allows us to connect to our Postgres instance for storing data.
 export async function getClient() {
-    const client = new Client("postgres://avnadmin:AVNS_Dd5xPtPAxXcMAt2hQwO@pg-2cbdf470-mohammedzee98-2970.h.aivencloud.com:17452/defaultdb?sslmode=require");
+    const client = new Client(`postgres://avnadmin:${key}@pg-2cbdf470-mohammedzee98-2970.h.aivencloud.com:17452/defaultdb?sslmode=require`);
     await client.connect();
     return client;
 }
+
+console.log(key);
