b911ea12265888ee66138f54edc157c86c711258
Mohammed Zaid <mohammedzee98@gmail.com>
2024-08-25 14:22:18 +0530

    basic running of CloudFlare app

--- Deploy/my-app/src/index.ts
+++ Deploy/my-app/src/index.ts
@@ -1,18 +1,12 @@
-/**
- * Welcome to Cloudflare Workers! This is your first worker.
- *
- * - Run `npm run dev` in your terminal to start a development server
- * - Open a browser tab at http://localhost:8787/ to see your worker in action
- * - Run `npm run deploy` to publish your worker
- *
- * Bind resources to your worker in `wrangler.toml`. After adding bindings, a type definition for the
- * `Env` object can be regenerated with `npm run cf-typegen`.
- *
- * Learn more at https://developers.cloudflare.com/workers/
- */
+
+export interface Env {
+
+}
 
 export default {
-	async fetch(request, env, ctx): Promise<Response> {
-		return new Response('Hello World!');
+	async fetch(request: Request, env: Env, ctx: ExecutionContext) {
+		return Response.json({
+			message: "Hi there, this your boy Zaidy"
+		});
 	},
-} satisfies ExportedHandler<Env>;
+}
