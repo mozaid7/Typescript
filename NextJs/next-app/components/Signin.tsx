44f70b2d5c76996882bad16f5383dd5c82333560
Mohammed Zaid <mohammedzee98@gmail.com>
2024-10-10 21:29:53 +0530

    "use client" used

--- NextJs/next-app/components/Signin.tsx
+++ NextJs/next-app/components/Signin.tsx
@@ -1,3 +1,4 @@
+import { Button } from "./Button";
 
 export function SigninComponent() {
     return <div className="h-screen flex justify-center flex-col">
@@ -12,9 +13,7 @@ export function SigninComponent() {
                 <div className="pt-2">
                     <LabelledInput label="Username" placeholder="example@gmail.com" />
                     <LabelledInput label="Password" type={"password"} placeholder="example123"/>
-                    <button 
-                        type="button" className="mt-8 w-full text-white bg-gray-800 focus:ring-4 focus:ring-gray-300 font-medium rounded-lg text-sm px-5 py-2.5 me-2 mb-2 ">Sign in
-                    </button>
+                    <Button />
                 </div>
             </div>
         </a>
