4f28a86acabec4e47d627d1fa9cce966e61dc982
Mohammed Zaid <mohammedzee98@gmail.com>
2024-10-14 21:56:31 +0530

    Db added and Signup route created

--- NextJs/next-app2/app/page.tsx
+++ NextJs/next-app2/app/page.tsx
@@ -1,24 +1,8 @@
-import axios from "axios";
 
-async function getUserDetails() {
-    const response = await axios.get("http://localhost:3000/api/user")
-    return response.data;
-}
-
-// async component
-export default async function Home() {
-    const userDetails = await getUserDetails();
-
-    return <div className="flex flex-col justify-center h-screen">
-        <div className="flex justify-center">
-            <div className="border p-8 rounded">
-                <div>
-                    Name: {userDetails?.name}
-                </div>
-                
-                    Email: {userDetails?.email}
-            </div>
-        </div>
-    </div>
-}
-
+export default function Home() {
+    return (
+      <div>
+        hi there
+      </div>
+    );
+  }
