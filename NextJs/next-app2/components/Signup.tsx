132373a55eab028d388a6b87219f61b8a276fde7
Mohammed Zaid <mohammedzee98@gmail.com>
2024-10-15 22:10:08 +0530

    server action created

--- NextJs/next-app2/components/Signup.tsx
+++ NextJs/next-app2/components/Signup.tsx
@@ -1,8 +1,8 @@
 "use client"
 
-import axios from "axios";
 import { ChangeEventHandler, useState } from "react";
 import { useRouter } from "next/navigation";
+import { signup } from "@/app/actions/user";
 
 export function Signup() {
     const [username, setUsername] = useState("");
@@ -26,12 +26,8 @@ export function Signup() {
                             setPassword(e.target.value)
                         }} label="Password" type={"password"} placeholder="123456" />
 
-                        <button onClick={async () => {
-                            await axios.post("http://localhost:3000/api/user", {
-                                username,
-                                password
-                            });
-                            router.push("/")
+                        <button onClick={() => {
+                            signup(username, password)
                         }} type="button" className="mt-8 w-full text-white bg-gray-800 focus:ring-4 focus:ring-gray-300 font-medium rounded-lg text-sm px-5 py-2.5 me-2 mb-2">Sign in</button>
                     </div>
                 </div>
