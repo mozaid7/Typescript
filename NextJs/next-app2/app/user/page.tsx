39407c91110e0b123b459b92b5b54a99f851c385
Mohammed Zaid <mohammedzee98@gmail.com>
2024-10-15 21:42:41 +0530

    UI edited

--- NextJs/next-app2/app/user/page.tsx
+++ NextJs/next-app2/app/user/page.tsx
@@ -15,9 +15,15 @@ export default async function User() {
     const data = await fetchData();
 
     return (
-        <div>
-            {data.name}
-            {data.username}
+        <div className="flex flex-col justify-center h-screen">
+            <div className="flex justify-center">
+                <div className="border p-8 rounded">
+                    <div>
+                        Name: {data?.name}
+                    </div>
+                    {data?.username}
+                </div>
+            </div>
         </div>
     )
 }
