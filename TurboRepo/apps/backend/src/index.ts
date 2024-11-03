9775e6e7a611ce0102a1dc077b5f25b5704aa307
Mohammed Zaid <mohammedzee98@gmail.com>
2024-11-03 17:47:08 +0530

    imported from the common module to backend module

--- TurboRepo/apps/backend/src/index.ts
+++ TurboRepo/apps/backend/src/index.ts
@@ -1,6 +1,8 @@
 import express from "express";
 const app = express();
+import { BACKEND_URL } from "@repo/common/config";
 
+console.log(BACKEND_URL);
 app.use("/",(req,res) => {
     res.json({
         msg: "Hello from Backend express"
