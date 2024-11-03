66899b7e00c789fb7e015870a269f3d48e8e490a
Mohammed Zaid <mohammedzee98@gmail.com>
2024-11-03 15:41:59 +0530

    input comp added

--- TurboRepo/apps/web/app/admin/page.tsx
+++ TurboRepo/apps/web/app/admin/page.tsx
@@ -1,8 +1,11 @@
 import { Admin } from "@repo/ui/admin";
+import { InputBox } from "@repo/ui/input-box";
+
 
 export default function() {
     return <div>
         Hello this is Zaid.
         <Admin />
+        <InputBox />
     </div>
 }
---
+++ TurboRepo/packages/ui/src/input-box.tsx
@@ -0,0 +1,10 @@
+
+export const InputBox = () => {
+  return (
+    <div>
+      Hi There !
+      <input type={"text"} placeholder="Type your text"></input>
+      Hi There !
+    </div>
+  );
+};
