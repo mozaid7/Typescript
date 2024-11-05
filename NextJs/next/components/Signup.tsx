a720ffab0f5dfd29ace8458cea00f77105fe3a92
Mohammed Zaid <mohammedzee98@gmail.com>
2024-11-05 16:30:23 +0530

    router used

--- NextJs/next/components/Signup.tsx
+++ NextJs/next/components/Signup.tsx
@@ -27,10 +27,11 @@ export function SignupComponent() {
                         }} label="Password" type={"password"} placeholder="123456" />
 
                         <button onClick={async () => {
-                            const response = await axios.post("http://localhost:3000/api/user" , {
+                            await axios.post("http://localhost:3000/api/user" , {
                                 username,
                                 password
                             });
+                            router.push("/")
                         }} type="button" className="mt-8 w-full text-white bg-gray-800 focus:ring-4 focus:ring-gray-300 font-medium rounded-lg text-sm px-5 py-2.5 me-2 mb-2">Sign in</button>
                     </div>
                 </div>
