2437c636fb8e62a7c3dbc418e3d07e9e3b2c8f59
Mohammed Zaid <mohammedzee98@gmail.com>
2024-11-08 20:40:08 +0530

    catching routes

--- NextJs/next-auth/app/api/auth/[...authRoutes]/route.ts
+++ NextJs/next-auth/app/api/auth/[...authRoutes]/route.ts
@@ -1,6 +1,7 @@
-import { NextResponse } from "next/server";
+import { NextRequest, NextResponse } from "next/server";
 
-export function GET() {
+export async function GET(req: NextRequest, arg: any) {
+    await console.log(arg.params.authRoutes); // shows the current route we are on
     return NextResponse.json({
         msg: "You are logged !!"
     })
