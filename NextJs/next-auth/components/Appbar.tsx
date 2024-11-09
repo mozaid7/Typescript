421c4ba00f5c2c50b9640bf82bab2bad483af6ca
Mohammed Zaid <mohammedzee98@gmail.com>
2024-11-09 19:05:04 +0530

    no router needed

--- NextJs/next-auth/components/Appbar.tsx
+++ NextJs/next-auth/components/Appbar.tsx
@@ -1,9 +1,7 @@
 "use client"
-import { useRouter } from "next/navigation";
 import { signIn, signOut } from "next-auth/react";
 
 export const Appbar = () => {
-    const router = useRouter();
     return <div>
         <button onClick={() => {
            signIn();
