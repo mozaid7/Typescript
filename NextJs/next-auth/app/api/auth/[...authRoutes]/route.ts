db514111d5aee244e24ce22faa700a2d47bf4dcb
Mohammed Zaid <mohammedzee98@gmail.com>
2024-11-09 08:58:43 +0530

    next auth init and catched

--- NextJs/next-auth/app/api/auth/[...authRoutes]/route.ts
+++
@@ -1,8 +0,0 @@
-import { NextRequest, NextResponse } from "next/server";
-
-export async function GET(req: NextRequest, arg: any) {
-    await console.log(arg.params.authRoutes); // shows the current route we are on
-    return NextResponse.json({
-        msg: "You are logged !!"
-    })
-}
