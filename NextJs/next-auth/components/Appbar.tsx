438af79bab3e5f1458e4a8ecc351468f262a8666
Mohammed Zaid <mohammedzee98@gmail.com>
2024-11-09 19:03:38 +0530

    signin & signout hook used

--- NextJs/next-auth/components/Appbar.tsx
+++ NextJs/next-auth/components/Appbar.tsx
@@ -1,11 +1,16 @@
 "use client"
 import { useRouter } from "next/navigation";
+import { signIn, signOut } from "next-auth/react";
 
 export const Appbar = () => {
     const router = useRouter();
     return <div>
         <button onClick={() => {
-            router.push("/api/auth/signin");
+           signIn();
         }}>SignIn</button>
+
+        <button onClick={() => {
+           signOut();
+        }}>Logout</button>
     </div>
 }
