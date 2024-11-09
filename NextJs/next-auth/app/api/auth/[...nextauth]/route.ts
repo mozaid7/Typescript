20df1b7df505208e9ee27b921dc472ec678be54d
Mohammed Zaid <mohammedzee98@gmail.com>
2024-11-09 20:53:19 +0530

    id displayed on server side

--- NextJs/next-auth/app/api/auth/[...nextauth]/route.ts
+++ NextJs/next-auth/app/api/auth/[...nextauth]/route.ts
@@ -1,45 +1,7 @@
+import { NEXT_AUTH } from "@/app/lib/auth";
 import NextAuth from "next-auth";
-import CredentialsProvider  from "next-auth/providers/credentials";
 
-const handler = NextAuth({
-    providers : [
-        CredentialsProvider({
-            name: "Email",
-            credentials: {
-                username: {label: 'Username', type:'text', placeholder:'example@.com'},
-                password: {label: 'Password', type:'password', placeholder:'12345'}
-            },
-            async authorize(credentials : any) {
-                // const username = credentials.username;
-                // const password = credentials.password;
-                console.log(credentials);
-                // validation
-                return {
-                    id: "1", // user.id
-                    name: "Zaid", // user.name
-                    email: "zaid@example.com" // user.username
-                };
-            },
-        })
-    ],
-    secret:process.env.NEXTAUTH_SECRET,
-    callbacks : {
-        // jwt: ({token,user}) => {
-        //   token.userId = token.sub;
-        //   console.log(token);
-        //    return token
-        // },
-        // session callback is more efficient than jwt callback
-        
-        session: ({session, token, user}: any) => {
-            console.log(session);
-            if (session && session.user) {
-                session.user.id = token.sub;
-            }
-            return session;
-        }
-    }
-});
+const handler = NextAuth(NEXT_AUTH);
 
 export const GET = handler;
 export const POST = handler;
--- NextJs/next-auth/app/user/page.tsx
+++ NextJs/next-auth/app/user/page.tsx
@@ -1,8 +1,11 @@
 import { getServerSession } from "next-auth";
+import { NEXT_AUTH } from "../lib/auth";
+import { Appbar } from "@/components/Appbar";
 
 export default async function() {
-    const session = await getServerSession();
+    const session = await getServerSession(NEXT_AUTH);
     return <div>
+        <Appbar />
         User Details on Server 
         {JSON.stringify(session)};
     </div>
